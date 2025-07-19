0ff3c4f185b34906d959d798bc1513d8975bf9d6
kitakou0313 <kitakou0313@gmail.com>
2025-07-19 21:09:16 +0900

    add: stdinを追加

--- src/tenkei1.ts
+++ src/tenkei1.ts
@@ -1,3 +1,12 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').split(' ');
+function solve(input: string) {
+    const [a, b, c] = input.split('\n')
+    const [N, L] = a.split(' ').map(Number);
+    const K = Number(b)
+    const Ai = c.split(' ').map(Number)
 
-console.log(input)
+
+    console.log(N, L, K, Ai)
+}
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
+solve(input)
