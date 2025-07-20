0f10bcbb60e25241b0e083fc24d638e370da8d84
kitakou0313 <kitakou0313@gmail.com>
2025-07-20 23:47:27 +0900

    add: add test

--- src/1/tenkei1.ts
+++ src/1/tenkei1.ts
@@ -1,11 +1,26 @@
+function isEnableThatAllYokasLengthLognerThanX(): boolean {
+    return true
+}
+
 function solve(input: string) {
     const [a, b, c] = input.split('\n')
     const [N, L] = a.split(' ').map(Number);
     const K = Number(b)
     const Ai = c.split(' ').map(Number)
 
+    // 二分探索で最小値の最大化を行う
+    let left = 0;
+    let right = L + 1;
 
-    console.log(N, L, K, Ai)
+    while (right - left > 1) {
+        const mid = Math.floor((left + right) / 2)
+
+        if (isEnableThatAllYokasLengthLognerThanX()){
+            left = mid
+        }else{
+            right = mid
+        }
+    }
 }
 
 const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
