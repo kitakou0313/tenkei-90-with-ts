f18b430aa2077b4ae5d093b76b96d80e89593494
kitakou0313 <kitakou0313@gmail.com>
2025-07-21 18:19:50 +0900

    add: add ans of 1

--- src/1/tenkei1.ts
+++ src/1/tenkei1.ts
@@ -1,27 +1,43 @@
-function isEnableThatAllYokasLengthLognerThanX(): boolean {
-    return true
+function isEnableThatAllYokansLengthLognerThanX(Ai: number[], K: number, trgLength: number, L:number): boolean {
+    let countOfCuttedYoukan = 0;
+    let lengthOfYokanCutted = 0;
+
+    for (let i = 0; i < Ai.length; i++) {
+        if(Ai[i] - lengthOfYokanCutted >= trgLength){
+            countOfCuttedYoukan += 1
+            lengthOfYokanCutted = Ai[i]
+        }
+    }
+    if (L - lengthOfYokanCutted >= trgLength) {
+        countOfCuttedYoukan += 1
+    }
+    
+    return countOfCuttedYoukan >= K + 1;
 }
 
-function solve(input: string) {
+function solve(input: string):number {
     const [a, b, c] = input.split('\n')
     const [N, L] = a.split(' ').map(Number);
     const K = Number(b)
     const Ai = c.split(' ').map(Number)
 
     // 二分探索で最小値の最大化を行う
-    let left = 0;
-    let right = L + 1;
+    let left = 0; // 可能な条件の最大値
+    let right = L + 1; // 不可能な条件の最小値
+    let mid = right;
 
-    while (right - left > 1) {
-        const mid = Math.floor((left + right) / 2)
+    while (right - left > 1) { // これらが隣り合う値になる -> 差が1になるまで探索する
+        mid = Math.floor((left + right) / 2)
 
-        if (isEnableThatAllYokasLengthLognerThanX()){
+        if (isEnableThatAllYokansLengthLognerThanX(Ai, K, mid, L)){
             left = mid
         }else{
             right = mid
         }
     }
+
+    return left
 }
 
 const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
-solve(input)
+console.log(solve(input))
