510d70451d6d82f57f4899c096598f39c6617bc8
kitakou0313 <kitakou0313@gmail.com>
2025-08-01 08:08:25 +0900

    add: add init for tenkei4

--- src/4/tenkei4.ts
+++ src/4/tenkei4.ts
@@ -1,5 +1,5 @@
 function solve4(inputs:string[]) {
-    const [H, V] = inputs.map((char) => {return parseInt(char, 10)})
+    const [H, W] = inputs[0].split(' ').map((char) => {return parseInt(char, 10)})
     const masu: number[][] = []
     for (let index = 1; index <= H; index++) {
         masu.push(
@@ -7,7 +7,26 @@ function solve4(inputs:string[]) {
         )
     }
 
-    console.log(masu)
+    // 各行ごとの合計
+    const sumOfRows: number[] = []
+    for (const row of masu) {
+        sumOfRows.push(row.reduce((acc, element) => {
+            return acc + element
+        }, 0))
+    }
+
+    // 各列ごとの合計
+    const sumOfColumn: number[] = []
+    for (let rowInd = 0; rowInd < W; rowInd++) {
+        let sum = 0
+        for (let columnInd = 0; columnInd < H; columnInd++) {
+            sum += masu[columnInd][rowInd]
+        }
+        sumOfColumn.push(sum)
+    }
+
+    console.log(sumOfRows)
+    console.log(sumOfColumn)
 }
 
 
