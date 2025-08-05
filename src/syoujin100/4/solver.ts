89a6c64199ac994c013b77e5309ed633702c62f3
kitakou0313 <kitakou0313@gmail.com>
2025-08-06 08:11:35 +0900

    add: add ans

--- src/syoujin100/4/solver.ts
+++ src/syoujin100/4/solver.ts
@@ -5,8 +5,20 @@ function solveSyoujin100_4(inputs:string[]) {
         A_matrix.push(inputs[index].split(" ").map((x) => {return parseInt(x, 10)}))
     }
 
-    console.log(N, M)
-    console.log(A_matrix)
+    let maxScore = -1
+    for (let m1 = 0; m1 < M - 1; m1++) {
+        for (let m2 = m1+1; m2 < M; m2++) {
+            let scoreInCaseM1AndM2 = 0
+            for (let n = 0; n < N; n++) {
+                scoreInCaseM1AndM2 += Math.max(
+                    A_matrix[n][m1], A_matrix[n][m2]
+                )
+            }
+            maxScore = Math.max(maxScore, scoreInCaseM1AndM2)
+        }
+    }
+
+    console.log(maxScore)
 }
 
 const inputSyoujin100_4 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
