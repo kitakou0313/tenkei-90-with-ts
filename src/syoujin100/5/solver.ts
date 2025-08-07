8818409ae99dd1608b89765bf800dc8f9b1153ee
kitakou0313 <kitakou0313@gmail.com>
2025-08-08 08:21:55 +0900

    add: add ans of syoujin 100

--- src/syoujin100/5/solver.ts
+++ src/syoujin100/5/solver.ts
@@ -1,34 +1,36 @@
 function solveSyoujin100_5(inputs:string[]) {
-    const [A,B,C,X,Y] = inputs[0].split(" ").map(
+    const [priceOfAPizza, priceOfBPizza, priceOfABPizza, trgNumberOfAPizza, trgNumberOfBPizza] = inputs[0].split(" ").map(
         (char) => {return parseInt(char, 10)}
     )
-    
-    const trgNumberOfX = Math.max(X, Y)
-    const trgNumberOfY = Math.min(X, Y)
 
     // ABピザの買い方ごとに試す
     let minCost = Infinity
 
     // Xピザを全てABピザで買う
-    minCost = Math.max(
-        minCost, trgNumberOfX * C * 2
-    )
-
-    // Yピザを全てABピザで買う
-    if (X > Y) {
-        minCost = Math.max(
-            minCost,  * C * 2 + (A - B) * X
+    if (trgNumberOfAPizza > trgNumberOfBPizza) {
+        minCost = Math.min(
+            minCost,  trgNumberOfAPizza * priceOfABPizza * 2
         )
     }else{
-        minCost = Math.max(
-            minCost, 
+        minCost = Math.min(
+            minCost, trgNumberOfBPizza * priceOfABPizza * 2
         )
     }
 
+    // Yピザを全てABピザで買う
+    if (trgNumberOfAPizza > trgNumberOfBPizza) {
+        minCost = Math.min(
+            minCost,  (trgNumberOfAPizza - trgNumberOfBPizza) * priceOfAPizza + trgNumberOfBPizza * priceOfABPizza * 2
+        )
+    }else{
+        minCost = Math.min(
+            minCost, (trgNumberOfBPizza - trgNumberOfAPizza) * priceOfBPizza + trgNumberOfAPizza * priceOfABPizza * 2
+        )
+    }
 
     // 0ピザを全てABピザで買う
-    minCost = Math.max(
-        minCost, A * X + B * Y
+    minCost = Math.min(
+        minCost, priceOfAPizza * trgNumberOfAPizza + priceOfBPizza * trgNumberOfBPizza
     )
 
     console.log(minCost)
