42816df15b027db52f3732028799204a12282f3e
kitakou0313 <kitakou0313@gmail.com>
2025-08-08 08:13:52 +0900

    add: add consideration and code

--- src/syoujin100/5/solver.ts
+++ src/syoujin100/5/solver.ts
@@ -2,7 +2,36 @@ function solveSyoujin100_5(inputs:string[]) {
     const [A,B,C,X,Y] = inputs[0].split(" ").map(
         (char) => {return parseInt(char, 10)}
     )
-    console.log(A, B, C, X, Y)
+    
+    const trgNumberOfX = Math.max(X, Y)
+    const trgNumberOfY = Math.min(X, Y)
+
+    // ABピザの買い方ごとに試す
+    let minCost = Infinity
+
+    // Xピザを全てABピザで買う
+    minCost = Math.max(
+        minCost, trgNumberOfX * C * 2
+    )
+
+    // Yピザを全てABピザで買う
+    if (X > Y) {
+        minCost = Math.max(
+            minCost,  * C * 2 + (A - B) * X
+        )
+    }else{
+        minCost = Math.max(
+            minCost, 
+        )
+    }
+
+
+    // 0ピザを全てABピザで買う
+    minCost = Math.max(
+        minCost, A * X + B * Y
+    )
+
+    console.log(minCost)
 }
 
 const inputSyoujin100_5 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
