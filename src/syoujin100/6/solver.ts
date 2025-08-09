8f205c39d04a0513c1bfbd97d5c38843cb14ed88
kitakou0313 <kitakou0313@gmail.com>
2025-08-09 22:16:48 +0900

    add: add template for 5

---
+++ src/syoujin100/6/solver.ts
@@ -0,0 +1,8 @@
+function solveSyoujin100_6(inputs:string[]) {
+    const N = parseInt(inputs[0], 10)
+    const S = inputs[1]
+
+}
+
+const inputSyoujin100_6 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveSyoujin100_6(inputTemplate)
