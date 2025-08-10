5bfb36839a373a48bb6b1b09a7fc08cd199c10db
kitakou0313 <kitakou0313@gmail.com>
2025-08-10 22:49:26 +0900

    add: add plot of algo

--- src/syoujin100/6/solver.ts
+++ src/syoujin100/6/solver.ts
@@ -2,7 +2,26 @@ function solveSyoujin100_6(inputs:string[]) {
     const N = parseInt(inputs[0], 10)
     const S = inputs[1]
 
+    const allSelectionOfDigitCombinationsNumber = N * (N - 1) * (N - 2) / 6
+
+    let duplicateSelectioncount = 0
+
+    for (let trgLuckNumber = 0; trgLuckNumber <= 999; trgLuckNumber++) {
+        const trgLuckNumberStr = trgLuckNumber.toString(10).padStart(3, "0")
+
+        const resOfCountSelectionOfDigitCreatingTrgLucyNumber = countSelectionOfDigitCreatingTrgLucyNumber(trgLuckNumberStr)
+
+        duplicateSelectioncount = resOfCountSelectionOfDigitCreatingTrgLucyNumber == 0 ? duplicateSelectioncount : duplicateSelectioncount + (resOfCountSelectionOfDigitCreatingTrgLucyNumber - 1) 
+        
+    }
+
+    console.log(allSelectionOfDigitCombinationsNumber - duplicateSelectioncount)
+
+    function countSelectionOfDigitCreatingTrgLucyNumber(trgLuckNumberStr:string): number {
+        
+    }
+
 }
 
 const inputSyoujin100_6 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-solveSyoujin100_6(inputTemplate)
+solveSyoujin100_6(inputSyoujin100_6)
