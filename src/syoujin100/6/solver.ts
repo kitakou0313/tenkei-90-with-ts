16a47f2c8d9198fcc8dd59d686423b91ed2437e8
kitakou0313 <kitakou0313@gmail.com>
2025-08-11 23:15:59 +0900

    fix: fix code

--- src/syoujin100/6/solver.ts
+++ src/syoujin100/6/solver.ts
@@ -2,22 +2,19 @@ function solveSyoujin100_6(inputs:string[]) {
     const N = parseInt(inputs[0], 10)
     const S = inputs[1]
 
-    const allSelectionOfDigitCombinationsNumber = N * (N - 1) * (N - 2) / 6
+    const numberToIndexesMap: Map<string, Set<number>> = new Map()
+    for (let index = 0; index <= 9; index++) {
+        numberToIndexesMap.set(index.toString(), new Set<number>())   
+    }
 
-    let duplicateSelectioncount = 0
+    // 前処理
+    for (let index = 0; index < S.length; index++) {
+        numberToIndexesMap.get(S[index])?.add(index)
+    }
 
     for (let trgLuckNumber = 0; trgLuckNumber <= 999; trgLuckNumber++) {
         const trgLuckNumberStr = trgLuckNumber.toString(10).padStart(3, "0")
 
-        const resOfCountSelectionOfDigitCreatingTrgLucyNumber = countSelectionOfDigitCreatingTrgLucyNumber(trgLuckNumberStr)
-
-        duplicateSelectioncount += resOfCountSelectionOfDigitCreatingTrgLucyNumber == 0 ? 0 : (resOfCountSelectionOfDigitCreatingTrgLucyNumber - 1) 
-        
-    }
-
-    console.log(allSelectionOfDigitCombinationsNumber - duplicateSelectioncount)
-
-    function countSelectionOfDigitCreatingTrgLucyNumber(trgLuckNumberStr:string): number {
         
     }
 
