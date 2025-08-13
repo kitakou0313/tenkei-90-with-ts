90830efd526cea37888093c92a7d59172b861d94
kitakou0313 <kitakou0313@gmail.com>
2025-08-14 08:15:49 +0900

    add: add map

--- src/syoujin100/7/solver.ts
+++ src/syoujin100/7/solver.ts
@@ -1,8 +1,9 @@
 function solveSyoujin100_7(inputs:string[]) {
     const N = parseInt(inputs[0], 10)
-    const pillar_coordinates: number[][] = []
+    const pillar_coordinates_map = new Map<number, Map<number, boolean>>()
     for (let index = 1; index < inputs.length; index++) {
-        pillar_coordinates.push(inputs[index].split(" ").map((x) => {return parseInt(x, 10)}))
+        const [X, Y] = inputs[index].split(" ").map((x) => {return parseInt(x, 10)})
+        pillar_coordinates_map.set(X, new Map([[Y, true]]))
     }
 }
 
