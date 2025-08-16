289dc23cbf8786e1c0b0f693f831707fa91e9fbd
kitakou0313 <kitakou0313@gmail.com>
2025-08-16 20:54:33 +0900

    add: add function to valid if trg square exists

--- src/syoujin100/7/solver.ts
+++ src/syoujin100/7/solver.ts
@@ -17,16 +17,11 @@ function solveSyoujin100_7(inputs:string[]) {
             if (!(isExistSquareMadeWithPillarAandB(pillarACoordinate, pillarBCoordinate, pillarCoordinatesMap))) {
                 continue
             }
-            
 
             // 存在していれば正方形の面積を算出し、最大のものか更新
-            let distanceBetweenPillarAandPillarB = Math.sqrt(
-                (pillarACoordinate[0] - pillarBCoordinate[0])**2 + 
-                (pillarACoordinate[1] - pillarBCoordinate[1])**2
-            )
             sizeOfMostBiggestSquare = Math.max(
                 sizeOfMostBiggestSquare,
-                distanceBetweenPillarAandPillarB**2
+                (pillarACoordinate[0] - pillarBCoordinate[0])**2 + (pillarACoordinate[1] - pillarBCoordinate[1])**2
             )
         }
     }
@@ -34,9 +29,37 @@ function solveSyoujin100_7(inputs:string[]) {
     console.log(sizeOfMostBiggestSquare)
 
     function isExistSquareMadeWithPillarAandB(pillarACoordinate: number[], pillarBCoordinate: number[], pillarCoordinatesMap: Map<number, Map<number, boolean>>): boolean {
-        return true
+        // 一つの柱からできる正方形には2通りが考えられるが、面積は変わらない
+        // そのためどちらか片方が構成できれば構成できるとして返して良い
+        
+        // X軸の変量とY軸の変量を求める
+        let xDiff = pillarACoordinate[0] - pillarBCoordinate[0]
+        let yDiff = pillarACoordinate[1] - pillarBCoordinate[1]
+        let diffsBetweenAandB = [yDiff, xDiff]
+
+        // X軸のみ変量を反転させ、対応する柱が存在するか確認
+        let trgPillarFromACoordinates = [pillarACoordinate[0] - diffsBetweenAandB[0], pillarACoordinate[1] + diffsBetweenAandB[1]]
+        let trgPillarFromBCoordinates = [pillarBCoordinate[0] - diffsBetweenAandB[0], pillarBCoordinate[1] + diffsBetweenAandB[1]]
+        if (
+            pillarCoordinatesMap.get(trgPillarFromACoordinates[0])?.get(trgPillarFromACoordinates[1]) && 
+            pillarCoordinatesMap.get(trgPillarFromBCoordinates[0])?.get(trgPillarFromBCoordinates[1]) 
+        ) {
+            return true
+        }
+
+        // Y軸のみ変量を反転させ、対応する柱が存在するか確認
+        trgPillarFromACoordinates = [pillarACoordinate[0] + diffsBetweenAandB[0], pillarACoordinate[1] - diffsBetweenAandB[1]]
+        trgPillarFromBCoordinates = [pillarBCoordinate[0] + diffsBetweenAandB[0], pillarBCoordinate[1] - diffsBetweenAandB[1]]
+        if (
+            pillarCoordinatesMap.get(trgPillarFromACoordinates[0])?.get(trgPillarFromACoordinates[1]) && 
+            pillarCoordinatesMap.get(trgPillarFromBCoordinates[0])?.get(trgPillarFromBCoordinates[1]) 
+        ) {
+            return true
+        }
+
+        return false
     }
 }
 
 const inputSyoujin100_7 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-solve(inputSyoujin100_7)
+solveSyoujin100_7(inputSyoujin100_7)
