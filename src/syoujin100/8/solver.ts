371ba3c25afff8fe1f439de646c58fa99c1182b3
kitakou0313 <kitakou0313@gmail.com>
2025-08-19 08:16:05 +0900

    add: add consideration and code

--- src/syoujin100/8/solver.ts
+++ src/syoujin100/8/solver.ts
@@ -1,9 +1,33 @@
 function solveSyoujin8(inputs:string[]) {
     const N =  parseInt(inputs[0], 10)
+    const AGridsCustomerVisit: number[] = []
+    const BGridsCustomerVisit: number[] = []
     const gridsCustomerVisit: [number, number][] = []
+
     for (let index = 1; index < inputs.length; index++) {
         const [A, B] = inputs[index].split(" ").map((char) => {return parseInt(char, 10)})
+        AGridsCustomerVisit.push(A)
+        BGridsCustomerVisit.push(B)
         gridsCustomerVisit.push([A, B])
+    }
+
+    let minDistanceSum = Infinity
+    for (const entranceCandidate of AGridsCustomerVisit) {
+        for (const exitCandidate of BGridsCustomerVisit) {
+            minDistanceSum = Math.min(minDistanceSum, calcAllDistancesFromEntranceToExit(entranceCandidate, exitCandidate, gridsCustomerVisit))
+        }
+    }
+    console.log(minDistanceSum)
+
+    function calcAllDistancesFromEntranceToExit(entrance: number,exit: number, gridsCustomerVisit: [number, number][]) {
+        let distanceSum = 0
+        for (const grid of gridsCustomerVisit) {
+            distanceSum += Math.abs(entrance - grid[0])
+            distanceSum += Math.abs(grid[0] - grid[1])
+            distanceSum += Math.abs(grid[1] - exit)
+        }
+
+        return distanceSum
     }
 
 }
