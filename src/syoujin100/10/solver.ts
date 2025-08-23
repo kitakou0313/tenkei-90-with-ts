ea85f820b4666e7805aadc991bb7dce8db7f7a5b
kitakou0313 <kitakou0313@gmail.com>
2025-08-23 23:37:47 +0900

    add: add func to valid switch on off pattern

--- src/syoujin100/10/solver.ts
+++ src/syoujin100/10/solver.ts
@@ -9,12 +9,18 @@ function solveSyoujin100_10(inputs:string[]) {
 
     const pi = inputs[inputs.length-1].split(" ").map((char) => {return parseInt(char, 10)})
 
+    let numOfValidSwitchOnOffPattern = 0
     for (let bitNumber = 0; bitNumber < Math.pow(2, N); bitNumber++) {
         const switchNumToOnOffMapBasedBitNumber = convertBitNumberToOnOffPattern(bitNumber, N)
         console.log(switchNumToOnOffMapBasedBitNumber)
+        numOfValidSwitchOnOffPattern += isValidSwitchOnOffPattern(
+            switchNumToOnOffMapBasedBitNumber, 
+            pi,
+            lightAndConnectedSwitch
+            )? 1 : 0
     }
 
-    console.log(N, M, lightAndConnectedSwitch, pi)
+    console.log(numOfValidSwitchOnOffPattern)
 
     function convertBitNumberToOnOffPattern(bitNumber: number, N:number): Map<number, boolean> {
         const switchNumToOnOffMap = new Map<number, boolean>()
@@ -23,6 +29,20 @@ function solveSyoujin100_10(inputs:string[]) {
             switchNumToOnOffMap.set(shiftCount, ((bitNumber >> shiftCount) & 1) == 1 ? true:false)
         }
         return switchNumToOnOffMap
+    }
+
+    function isValidSwitchOnOffPattern(switchNumToOnOffMap: Map<number, boolean>, pi:number[], lightAndConnectedSwitch: Map<number, Set<number>>):boolean {
+        for (const [lightNumber, connectedSwitchSet] of lightAndConnectedSwitch) {
+            let onSwitchNum = 0
+            for (const connectedSwitch of connectedSwitchSet) {
+                onSwitchNum += switchNumToOnOffMap.get(connectedSwitch)? 1 : 0
+            }
+            if(onSwitchNum % 2 != pi[lightNumber]){
+                return false
+            }
+        }
+
+        return true
     }
 }
 
