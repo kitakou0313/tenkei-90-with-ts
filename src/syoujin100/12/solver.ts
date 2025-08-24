d9208cc143d37ecca6953a88bea1a15bc3a54560
kitakou0313 <kitakou0313@gmail.com>
2025-08-25 08:04:27 +0900

    add: add func to gen subset from bit

--- src/syoujin100/12/solver.ts
+++ src/syoujin100/12/solver.ts
@@ -7,6 +7,23 @@ function solveSyoujin100_12(inputs:string[]) {
             [x, y]
         )
     }
+
+    let maxFuctionSetSize = 0
+    for (let bitNumber = 0; bitNumber < Math.pow(2, N); bitNumber++) {
+        const fuctionSet = convertBitNumberToFuction(bitNumber, N)
+        maxFuctionSetSize = Math.max(maxFuctionSetSize, fuctionSet.size)
+    }
+    console.log(maxFuctionSetSize)
+
+    function convertBitNumberToFuction(bitNumber:number, N:number):Set<number> {
+        const fuctionSet = new Set<number>()
+        for (let shiftNumber = 0; shiftNumber < N; shiftNumber++) {
+            if (((bitNumber >> shiftNumber) & 1) == 1) {
+                fuctionSet.add(shiftNumber + 1)
+            }
+        }
+        return fuctionSet
+    }
 }
 
 const inputSyoujin100_12 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
