218fa43cf35fa467af5f8375baf211fbc7fb5b1b
kitakou0313 <kitakou0313@gmail.com>
2025-08-26 06:00:01 +0900

    add: add ans of syoujin 12

--- src/syoujin100/12/solver.ts
+++ src/syoujin100/12/solver.ts
@@ -1,17 +1,21 @@
 function solveSyoujin100_12(inputs:string[]) {
     const [N, M] = inputs[0].split(" ").map((char) => {return parseInt(char, 10)})
-    const relationships: [number, number][] = []
+    const relationships: boolean[][] = Array.from({ length: N }, () => 
+        Array.from({ length: N }, () => false)
+    );
     for (let lineNumber = 1; lineNumber < inputs.length; lineNumber++) {
         const [x, y] = inputs[lineNumber].split(" ").map((char) => {return parseInt(char, 10)})
-        relationships.push(
-            [x, y]
-        )
+        relationships[x-1][y-1] = true
+        relationships[y-1][x-1] = true
     }
 
     let maxFuctionSetSize = 0
     for (let bitNumber = 0; bitNumber < Math.pow(2, N); bitNumber++) {
         const fuctionSet = convertBitNumberToFuction(bitNumber, N)
-        maxFuctionSetSize = Math.max(maxFuctionSetSize, fuctionSet.size)
+        if(isFuctionValid(fuctionSet, relationships)){
+            maxFuctionSetSize = Math.max(maxFuctionSetSize, fuctionSet.size)
+        }
+
     }
     console.log(maxFuctionSetSize)
 
@@ -19,10 +23,25 @@ function solveSyoujin100_12(inputs:string[]) {
         const fuctionSet = new Set<number>()
         for (let shiftNumber = 0; shiftNumber < N; shiftNumber++) {
             if (((bitNumber >> shiftNumber) & 1) == 1) {
-                fuctionSet.add(shiftNumber + 1)
+                fuctionSet.add(shiftNumber)
             }
         }
         return fuctionSet
+    }
+
+    function isFuctionValid(fuctionSet: Set<number>, relationships:boolean[][]):boolean {
+        for (const giinA of fuctionSet) {
+            for (const giinB of fuctionSet) {
+                if (giinA == giinB) {
+                    continue
+                }
+                if(!relationships[giinA][giinB]){
+                    return false
+                }
+            }
+        }
+
+        return true
     }
 }
 
