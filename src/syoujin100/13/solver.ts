e977d748e35179f441cabcaed5691f7292a26ca5
kitakou0313 <kitakou0313@gmail.com>
2025-08-29 08:01:51 +0900

    add: add func to convert row number set from bit

--- src/syoujin100/13/solver.ts
+++ src/syoujin100/13/solver.ts
@@ -7,7 +7,20 @@ function solveSyoujin100_13(inputs:string[]) {
         )
     }
 
-    console.log(R, C, senbeiMap)
+    for (let bitNumber = 0; bitNumber < Math.pow(2, R); bitNumber++) {
+        const rowNumberSet = convertBitNumberToRowNumberSet(bitNumber, R)
+    }
+
+    function convertBitNumberToRowNumberSet(bitNumber: number, R: number): Set<number> {
+        const rowNumberSet = new Set<number>()
+        for (let shiftNumber = 0; shiftNumber < R; shiftNumber++) {
+            if (((bitNumber >> shiftNumber) & 1) == 1) {
+                rowNumberSet.add(shiftNumber)
+            }
+        }
+
+        return rowNumberSet
+    }
 }
 
 const inputSyoujin100_13 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
