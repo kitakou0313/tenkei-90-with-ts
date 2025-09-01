8a2eb9af32f24285bd851afaf210801a3906bb53
kitakou0313 <kitakou0313@gmail.com>
2025-09-02 08:16:21 +0900

    add: add ans

--- src/syoujin100/14/solver.ts
+++ src/syoujin100/14/solver.ts
@@ -5,6 +5,23 @@ function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
 function solveSyoujin14(inputs:string[]) {
     const [N, K] = parseSpaceSeparatedLineToNumberArray(inputs[0])
     const buildingHeights = parseSpaceSeparatedLineToNumberArray(inputs[1])
+
+    for (let bitNumber = 0; bitNumber < N; bitNumber++) {
+       
+        const selectedBuildingNumberSet = convertBitNumberToBuildingIndexSet(bitNumber, N)
+    }
+
+    function convertBitNumberToBuildingIndexSet(bitNumber:number, N:number):Set<number> {
+        const buildingIndexSet = new Set<number>()
+
+        for (let shiftNumber = 0; shiftNumber < N; shiftNumber++) {
+            if ((bitNumber >> shiftNumber & 1) == 1) {
+                buildingIndexSet.add(shiftNumber)
+            }
+        }
+
+        return buildingIndexSet
+    }
 }
 
 const inputSyoujin14 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
