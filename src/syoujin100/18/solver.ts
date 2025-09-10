a9e5e0097deb6634e6d7d1298a211f82883084e5
kitakou0313 <kitakou0313@gmail.com>
2025-09-11 08:24:25 +0900

    add: add ans

--- src/syoujin100/18/solver.ts
+++ src/syoujin100/18/solver.ts
@@ -7,19 +7,43 @@ function solveSyoujin18(inputs:string[]) {
     const [m] = parseSpaceSeparatedLineToNumberArray(inputs[1])
     const [n] = parseSpaceSeparatedLineToNumberArray(inputs[2])
 
-    const shopPoss: number[] = [] // 本店を含めていない点に注意
+    const shopPositions: number[] = [] // 本店を含めていない点に注意
     for (let line = 3; line < 3 + (n - 1); line++) {
         const [di] = parseSpaceSeparatedLineToNumberArray(inputs[line])
-        shopPoss.push(di)
+        shopPositions.push(di)
     }
-    const orderGoalPoss: number[] = []
+    const orderGoalPositions: number[] = []
     for (let line = n + 3; line < inputs.length; line++) {
         const [ki] = parseSpaceSeparatedLineToNumberArray(inputs[line])
-        orderGoalPoss.push(ki)
+        orderGoalPositions.push(ki)
     }
     // 末尾と先頭に本店を追加
-    shopPoss.unshift(0)
-    shopPoss.push(d)
+    shopPositions.push(0, d)
+    // お店を距離順にソート
+    shopPositions.sort()
+
+    let sumOfDistanceFromNearestShopAndOrderGoal = 0
+    for (const orderGoalPos of orderGoalPositions) {
+        
+    }
+
+    console.log(sumOfDistanceFromNearestShopAndOrderGoal)
+
+    
+    // 0 ~ 宅配先までで最も近いお店 と 宅配先 ~ 本店（末尾）までで最も近いお店を探索する
+    // その後それらのお店のうち近い方を発送元とする
+    function calcLeastDistanceBetweenShopAndShop(orderGoalPos: number, shopPositions:number[]): number {
+        // 配送先より小さい距離のお店を指す
+        let left = 0
+        // 配送先より大きい距離のお店をさす
+        let right = shopPositions.length - 1
+
+        while ((right - left) > 1) {
+            
+        }
+
+
+    }
 
 }
 
