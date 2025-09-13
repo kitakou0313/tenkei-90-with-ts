a112723b00e84f2953dbf96e886ceb754bc95889
kitakou0313 <kitakou0313@gmail.com>
2025-09-13 12:37:19 +0900

    add: add func

--- src/template/solver.ts
+++ src/template/solver.ts
@@ -2,6 +2,11 @@ function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
     return line.split(" ").map((char) => {return parseInt(char, 10)})
 }
 
+// 1行に1数字しかない場合にのみ使う
+function parseFirstNumber(line: string): number {
+    return parseInt(line, 10);
+}
+
 function solve(inputs:string[]) {
     
 }
