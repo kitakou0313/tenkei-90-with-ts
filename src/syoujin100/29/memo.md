8bc80192ee6ebf11f455e44d23446b42785177eb
kitakou0313 <kitakou0313@gmail.com>
2025-10-16 08:10:58 +0900

    add: add template

---
+++ src/syoujin100/29/memo.md
@@ -0,0 +1,13 @@
+# 問題リンク
+- https://atcoder.jp/contests/abc007/tasks/abc007_3
+
+## 考察
+- 制約
+- 要約
+- 考察
+
+## メモ
+
+## ToDo
+
+## 解説
---
+++ src/syoujin100/29/solver.ts
@@ -0,0 +1,58 @@
+function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
+    return line.split(" ").map((char) => {return parseInt(char, 10)})
+}
+
+// 1行に1数字しかない場合にのみ使う
+function parseFirstNumber(line: string): number {
+    return parseInt(line, 10);
+}
+
+function solveSyoujin29(inputs:string[]) {
+    class Queue<T> {
+        private items: T[] = [];
+	    private front: number = 0; // 最も早く追加された要素の位置を示す
+	    private rear: number = 0; // 次に要素が入る位置を示す
+
+        push(item: T) {
+            this.items[this.rear] = item
+            this.rear += 1
+        }
+
+        pop(): T | undefined {
+            if (this.getLength() === 0) {
+                return undefined
+            }
+            const currentFrontItem = this.items[this.front]
+            this.front += 1
+            return currentFrontItem
+        }
+
+        getLength() {
+            return this.rear - this.front
+        }
+    }
+    const [R, C] = parseSpaceSeparatedLineToNumberArray(inputs[0])
+    const [startR, startC] = parseSpaceSeparatedLineToNumberArray(inputs[1]).map((value) => value-1)
+    const [goalR, goalC] = parseSpaceSeparatedLineToNumberArray(inputs[2]).map((value) => value-1)
+    const maze: string[][] = []
+    for (let lineNumber = 3; lineNumber < inputs.length; lineNumber++) {
+        const row = inputs[lineNumber].split('')
+        maze.push(row)
+    }
+
+    function convertPosToKey(r:number, c:number): string {
+        return `${r}-${c}`
+    }
+    function convertKeyToPos(key: string): [number, number] {
+        const [r, c] = key.split("-").map((char) => {return parseInt(char, 10)})
+        return [r, c]
+    }
+
+    const posToLeastDistanceMap = new Map<string, number>()
+    const queue = new Queue<string>()
+    const visitedPosSet = new Set<string>()
+
+}
+
+const inputSyoujin29 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveSyoujin29(inputSyoujin29)
