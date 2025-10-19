47bd2ad4d2d9ea3fce7ab60f659fd51e17b0806e
kitakou0313 <kitakou0313@gmail.com>
2025-10-20 08:25:19 +0900

    add: add ans

--- src/syoujin100/30/solver.ts
+++ src/syoujin100/30/solver.ts
@@ -15,8 +15,44 @@ function solveSyoujin30(inputs:string[]) {
             inputs[lineNumber].split("")
         ) 
     }
+    for (let h = 0; h < H; h++) {
+        for (let w = 0; w < W; w++) {
+            const valInGridhw = cityMap[h][w]
+            if (valInGridhw === "S") {
+                cityMap[h][w] = "0"
+            }
+        }
+    }
 
-    console.log(H, W, N, cityMap)
+    function convertPosToStr(h:number, w:number): string {
+        return `${h}-${w}`
+    }
+    function convertStrToPos(key: string): [number, number] {
+        const [h, w] = key.split("-").map((char) => {return parseInt(char, 10)})
+        return [h, w]
+    }
+
+    const cheeseNumberToFactoryPos = new Map<number, string>()
+    for (let h = 0; h < H; h++) {
+        for (let w = 0; w < W; w++) {
+            const valInGridhw = cityMap[h][w]
+            if (valInGridhw in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
+                const cheeseNumber = parseInt(valInGridhw, 10)
+                cheeseNumberToFactoryPos.set(
+                    cheeseNumber, convertPosToStr(h, w)
+                ) 
+            }
+        }
+    }
+    let sumOfLeastDistances = 0
+
+    for (let cheeseNumberInGoal = 1; cheeseNumberInGoal < N+1; cheeseNumberInGoal++) {
+        const cheeseNumberInStart = cheeseNumberInGoal - 1
+        
+    }
+
+    console.log(sumOfLeastDistances)
+
 }
 
 const inputSyoujin30 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
