8afe5ff11d4150a38b0c9998e9a9e31855c50ae1
kitakou0313 <kitakou0313@gmail.com>
2025-10-22 08:16:32 +0900

    add: add code

---
+++ src/syoujin100/31/solver.ts
@@ -0,0 +1,21 @@
+function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
+    return line.split(" ").map((char) => {return parseInt(char, 10)})
+}
+
+// 1行に1数字しかない場合にのみ使う
+function parseFirstNumber(line: string): number {
+    return parseInt(line, 10);
+}
+
+function solveSyoujin31(inputs:string[]) {
+    const [W, H] = parseSpaceSeparatedLineToNumberArray(inputs[0])
+    const grids: string[][] = []
+    for (let lineNumber = 1; lineNumber < inputs.length; lineNumber++) {
+        grids.push(
+            inputs[lineNumber].split("")
+        )
+    }
+}
+
+const inputSyoujin31 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveSyoujin31(inputSyoujin31)
