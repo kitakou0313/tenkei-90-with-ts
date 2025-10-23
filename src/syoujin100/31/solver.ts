b4ac5593931a171858b1233d6614020a5e948da4
kitakou0313 <kitakou0313@gmail.com>
2025-10-24 08:23:44 +0900

    add: add ans

--- src/syoujin100/31/solver.ts
+++ src/syoujin100/31/solver.ts
@@ -9,15 +9,27 @@ function parseFirstNumber(line: string): number {
 
 function solveSyoujin31(inputs:string[]) {
     const [W, H] = parseSpaceSeparatedLineToNumberArray(inputs[0])
-    const grids: string[][] = []
+    const rawGrids: string[][] = []
     for (let lineNumber = 1; lineNumber < inputs.length; lineNumber++) {
-        grids.push(
-            inputs[lineNumber].split("")
+        rawGrids.push(
+            inputs[lineNumber].split(" ")
         )
     }
+    const grids: ("0" | "1")[][] = Array.from({length : H + 2}, () => Array.from({length: W + 2}, () => "0"))
+    
+    for (let h = 0; h < H; h++) {
+        for (let w = 0; w < W; w++) {
+            grids[h+1][w+1] = rawGrids[h][w] as ("0" | "1")
+        }
+    }
+
+    console.log(rawGrids)
+    console.log(grids)
 
     const dw = [ 0, 1,1,1,0,-1]
     const dh = [-1,-1,0,1,1, 0]
+
+    // 建物に覆われているグリッドを求める
 
     for (let w = 0; w < W; w++) {
         for (let h = 0; h < H; h++) {
