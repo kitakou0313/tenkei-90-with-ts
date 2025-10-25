cefa6d5ccc8ac100010a3de2bae89c478f7e0f84
kitakou0313 <kitakou0313@gmail.com>
2025-10-25 15:32:32 +0900

    add: add code to mark grids

--- src/syoujin100/31/solver.ts
+++ src/syoujin100/31/solver.ts
@@ -8,6 +8,14 @@ function parseFirstNumber(line: string): number {
 }
 
 function solveSyoujin31(inputs:string[]) {
+    function convertPosToStr(h:number, w:number): string {
+        return `${h}-${w}`
+    }
+    function convertStrToPos(key: string): [number, number] {
+        const [h, w] = key.split("-").map((char) => {return parseInt(char, 10)})
+        return [h, w]
+    }
+
     const [W, H] = parseSpaceSeparatedLineToNumberArray(inputs[0])
     const rawGrids: string[][] = []
     for (let lineNumber = 1; lineNumber < inputs.length; lineNumber++) {
@@ -29,13 +37,45 @@ function solveSyoujin31(inputs:string[]) {
     const dw = [ 0, 1,1,1,0,-1]
     const dh = [-1,-1,0,1,1, 0]
 
-    // 建物に覆われているグリッドを求める
+    // 建物に囲まれているグリッドはすべて黒扱いとする
+    // 最初にすべて1(黒)とする
+    const gridsAfterMakingBuildings: ("0" | "1")[][] = Array.from({length: H + 2}, () => {
+        return Array.from({length: W + 2}, () => "1")
+    })
 
-    for (let w = 0; w < W; w++) {
-        for (let h = 0; h < H; h++) {
-        
-        }       
+    const visited = new Set<string>()
+    function bfs(h:number, w: number) {
+        if (visited.has(convertPosToStr(h, w))) {
+            return
+        }
+        if (grids[h][w] === "1") {
+            return
+        }
+        gridsAfterMakingBuildings[h][w] = "0"
+        visited.add(convertPosToStr(h, w))
+
+        for (let indexInd = 0; indexInd < 6; indexInd++) {
+            const nh = h + dh[indexInd]
+            const nw = w + dw[indexInd]
+
+            if (nh < 0 || H + 1 < nh ||
+                nw < 0 || W + 1 < nw ||
+                grids[nh][nw] === "1") {
+                continue
+            }
+            if (visited.has(convertPosToStr(h, w))) {
+                continue
+            }
+        }
     }
+    
+    for (let h = 0; h < H + 2; h++) {
+        for (let w = 0; w < W + 2; w++) {
+            bfs(h, w)
+        }
+    }
+
+    console.log(gridsAfterMakingBuildings)
 }
 
 const inputSyoujin31 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
