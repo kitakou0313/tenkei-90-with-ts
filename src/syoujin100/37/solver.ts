4f4a891823d7f0f655cfb9de5afe111bbc3fcf1e
kitakou0313 <kitakou0313@gmail.com>
2025-11-22 21:21:05 +0900

    add: add ans without DP

--- src/syoujin100/37/solver.ts
+++ src/syoujin100/37/solver.ts
@@ -10,6 +10,31 @@ function parseFirstNumber(line: string): number {
 function solveSyoujin37(inputs:string[]) {
     const [n, m] = parseSpaceSeparatedLineToNumberArray(inputs[0])
     const coins = parseSpaceSeparatedLineToNumberArray(inputs[1])
+
+    function calcMinCoinNumber(coins: number[], trgValue:number):number {
+        console.log(trgValue)
+        if (trgValue === 1) {
+            return 1
+        }
+        if(trgValue === 0){
+            return 0
+        }
+
+        let minCoinNum = Number.MAX_SAFE_INTEGER
+        for (const coin of coins) {
+            if (coin > trgValue) {
+                continue
+            }
+            minCoinNum = Math.min(
+                minCoinNum,
+                1 + calcMinCoinNumber(coins, trgValue - coin)
+            )
+        }
+
+        return minCoinNum
+    }
+
+    console.log(calcMinCoinNumber(coins, n))
 }
 
 const inputSyoujin37 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
