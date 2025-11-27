512ea58f9cbc4f3fe38c427205e566925833df13
kitakou0313 <kitakou0313@gmail.com>
2025-11-28 08:31:58 +0900

    add: add solver with recursive

--- src/syoujin100/38/solver.ts
+++ src/syoujin100/38/solver.ts
@@ -8,6 +8,29 @@ function parseFirstNumber(line: string): number {
 }
 
 function solveSyoujin38(inputs:string[]) {
+    function calcMaximumLCSLengthByRecursive(X:string, Y:string):number {
+        function helper(i: number, j: number):number {
+            const M = X.length
+            const N = Y.length
+
+            if (i === M || j == N) {
+                return 0
+            }
+
+            if (X[i] === Y[j]) {
+                return 1 + helper(i + 1, j + 1);
+            }
+
+            return Math.max(
+                helper(i + 1, j),
+                helper(i, j + 1)
+            )
+        }
+
+        return helper(0, 0)
+    }
+    
+    
     function calcLengthOfLCS(X:string, Y:string): number {
         const M = X.length
         const N = Y.length
