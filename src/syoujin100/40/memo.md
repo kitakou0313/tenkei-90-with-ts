5f550effbaa9566318031828781f170408cb12ca
kitakou0313 <kitakou0313@gmail.com>
2025-12-10 06:16:16 +0900

    add: add template for Q40

---
+++ src/syoujin100/40/memo.md
@@ -0,0 +1,13 @@
+# 問題リンク
+- https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_d
+
+## 考察
+- 制約
+- 要約
+- 考察
+
+## メモ
+
+## ToDo
+
+## 解説
---
+++ src/syoujin100/40/solver.ts
@@ -0,0 +1,28 @@
+function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
+    return line.split(" ").map((char) => {return parseInt(char, 10)})
+}
+
+// 1行に1数字しかない場合にのみ使う
+function parseFirstNumber(line: string): number {
+    return parseInt(line, 10);
+}
+
+function solveSyoujin40(inputs:string[]) {
+    type PastaSchedule = {
+        day: number, 
+        pastaType: 1 | 2 | 3
+    }
+    const [N, K] = parseSpaceSeparatedLineToNumberArray(inputs[0])
+    const pastaSchduleList: PastaSchedule[] = []
+    for (let k = 0; k < K+1; k++) {
+        const [Ai, Bi] = parseSpaceSeparatedLineToNumberArray(inputs[1+k])
+        pastaSchduleList.push({
+            day:Ai,
+            pastaType: Bi as 1 | 2 | 3
+        })
+        
+    }
+}
+
+const inputSyoujin40 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveSyoujin40(inputSyoujin40)
