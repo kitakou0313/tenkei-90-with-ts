f4794c89c7fecd9d80f9f06eb8417245a145de9d
kitakou0313 <kitakou0313@gmail.com>
2025-12-26 22:32:55 +0900

    add: add templates for Q41

---
+++ src/syoujin100/41/memo.md
@@ -0,0 +1,13 @@
+# 問題リンク
+- https://atcoder.jp/contests/joi2013yo/tasks/joi2013yo_d
+
+## 考察
+- 制約
+- 要約
+- 考察
+
+## メモ
+
+## ToDo
+
+## 解説
---
+++ src/syoujin100/41/solver.ts
@@ -0,0 +1,26 @@
+function parseSpaceSeparatedLineToNumberArray(line:string): number[] {
+    return line.split(" ").map((char) => {return parseInt(char, 10)})
+}
+
+// 1行に1数字しかない場合にのみ使う
+function parseFirstNumber(line: string): number {
+    return parseInt(line, 10);
+}
+
+function solveSyoujin41(inputs:string[]) {
+    const [D, N] = parseSpaceSeparatedLineToNumberArray(inputs[0])
+    const templeturesList: number[] = []
+    const clothesParams :[number, number, number][] = []
+    for (let lineNumber = 1; lineNumber < 1+D; lineNumber++) {
+        const Ti = parseFirstNumber(inputs[lineNumber])
+        templeturesList.push(Ti)
+    }
+    for (let lineNumber = 1+D; lineNumber < 1+D+N; lineNumber++) {
+        const [Ai, Bi, Ci] = parseSpaceSeparatedLineToNumberArray(inputs[lineNumber])
+        clothesParams.push([Ai, Bi, Ci])
+    }
+
+}
+
+const inputSyoujin41 = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveSyoujin41(inputSyoujin41)
