fb1b2e1f29230b08991768762807734904b5579a
kitakou0313 <kitakou0313@gmail.com>
2026-03-24 08:28:09 +0900

    add: add ans

--- src/abc/450/solver.ts
+++ src/abc/450/solver.ts
@@ -7,16 +7,33 @@ function parseFirstNumber(line: string): number {
     return parseInt(line, 10);
 }
 
-function solveABC450a(inputs:string[]) {
+function solveABC450b(inputs:string[]) {
     const N = parseFirstNumber(inputs[0])
+    const Cij: number[][] = []
 
-    let resStr = ""
-    for (let number = N; number > 0; number--) {
-        resStr += `${number},`
+    for (let lineNumber = 1; lineNumber < 1+N-1; lineNumber++) {
+        const cij = parseSpaceSeparatedLineToNumberArray(inputs[lineNumber])
+        Cij.push(cij)
     }
 
-    console.log(resStr.slice(0, resStr.length-1))
+    // a, b, cを全通り試してバリデーションでもよかった
+    for (let a = 0; a < N-1; a++) {
+        for (let c = a+1; c < N-1; c++) {
+            const costAtoC = Cij[a][c]
+            for (let b = a+1; b < c; b++) {
+                const costAtoBtoC = Cij[a][b] + Cij[b][c]
+
+                if (costAtoBtoC < costAtoC) {
+                    console.log("Yes")
+                }
+            }
+        }
+        
+    }
+
+    console.log("No")
+
 }
 
-const inputABC450a = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
-solveABC450a(inputABC450a)
+const inputABC450b = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+solveABC450b(inputABC450b)
