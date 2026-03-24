f9e26a68d45f62c2baf1d1ed120a03984b0ecbb4
kitakou0313 <kitakou0313@gmail.com>
2026-03-25 08:36:58 +0900

    add: add ans

--- src/abc/450/solver.ts
+++ src/abc/450/solver.ts
@@ -7,31 +7,38 @@ function parseFirstNumber(line: string): number {
     return parseInt(line, 10);
 }
 
+function searchAns(Cij:number[][], N:number):string {
+    for (let a = 0; a < N-1; a++) {
+        for (let c = a+1; c < N; c++) {
+            const costAtoC = Cij[a][c]
+            for (let b = a+1; b < c; b++) {
+                const costAtoBtoC = Cij[a][b] + Cij[b][c]
+                if (costAtoBtoC < costAtoC) {
+                    return "Yes"
+                }
+            }
+        }
+    }
+
+    return "No"
+}
+
 function solveABC450b(inputs:string[]) {
     const N = parseFirstNumber(inputs[0])
     const Cij: number[][] = []
 
     for (let lineNumber = 1; lineNumber < 1+N-1; lineNumber++) {
         const cij = parseSpaceSeparatedLineToNumberArray(inputs[lineNumber])
-        Cij.push(cij)
+        const tmp0Array: number[] = []
+        for (let i = 0; i < lineNumber; i++) {
+            tmp0Array.push(0)
+        }
+        const extendedcij = tmp0Array.concat(cij)
+        Cij.push(extendedcij)
     }
 
     // a, b, cを全通り試してバリデーションでもよかった
-    for (let a = 0; a < N-1; a++) {
-        for (let c = a+1; c < N-1; c++) {
-            const costAtoC = Cij[a][c]
-            for (let b = a+1; b < c; b++) {
-                const costAtoBtoC = Cij[a][b] + Cij[b][c]
-
-                if (costAtoBtoC < costAtoC) {
-                    console.log("Yes")
-                }
-            }
-        }
-        
-    }
-
-    console.log("No")
+    console.log(searchAns(Cij, N))
 
 }
 
